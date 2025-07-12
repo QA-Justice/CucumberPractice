06c49c09eaf7157fca4144909cb6cce839d4fc0d
Tia <=>
2025-07-12 23:46:18 +0900

    Initial commit

---
+++ src/test/java/stepdefinitions/RunCucumberTest.java
@@ -0,0 +1,15 @@
+package stepdefinitions;
+
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
+
+import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
+
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("io/features")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
+public class RunCucumberTest {
+}
